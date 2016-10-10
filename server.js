9ef184bf95bd4b32ed4e275f3b8686d24a761079
vksbhrdwj2 <vksbhrdwj2@gmail.com>
2016-10-10 14:22:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/:articleName', function (req,res) {
-res.send(createTemplate(articles(articleName)));
+res.send(createTemplate(articles[articleName]));
 var articleName=req.params.articleName;
 });
 app.get('/ui/madi.png', function (req, res) {
