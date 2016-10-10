363593c0b2a91858264b088b78d1c6db5ece7436
vksbhrdwj2 <vksbhrdwj2@gmail.com>
2016-10-10 11:55:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/article1', function (req,res) {
+res.send('article1 requested will be served here');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
